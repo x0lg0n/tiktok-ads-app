bd28ea6efcb6001cc1de110c39dbd9e57fef4d4f
x0lg0n <kumarsiddharthakain@gmail.com>
2026-02-03 17:37:05 +0530

    fix: force rebuild for textarea visibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'bg-gray-50',
+    'border-gray-300',
+    'bg-red-50',
+    'border-red-500',
+  ],
   theme: {
     extend: {},
   },
